b38dd25e8e53c3658399d4296ad17e40aad306b3
kevinchap <kevin.chapron@hetic.net>
2015-08-21 01:59:56 +0200

    [Home - A propos - Solutions - Expertises] Integration responsive and animation

---
+++ js/script.js
@@ -0,0 +1,89 @@
+"use strict";
+
+$(function(){
+    // nav background change on scroll
+	$(window).scroll(function (event) {
+        if($(window).scrollTop()>0){
+            $('nav').addClass('on');
+        }else{
+            $('nav').removeClass('on');
+        }
+    });
+
+    // menu responsive and menu-button animation
+    if($('.menu-button')){
+        $('.menu-button').click(function(){
+            $('nav').toggleClass('active');
+            if($(this).hasClass('active')){
+                $(this).removeClass('rotate');
+                setTimeout(function(el){$(el).removeClass('active');},500, this);
+            }else{
+                $(this).addClass('active');
+                setTimeout(function(el){$(el).addClass('rotate');},500, this);
+            }
+        }); 
+    }
+
+    // HOME TEMOIGNAGE SLIDER
+        // puce navigation
+        $('section.four ul li .puce').click(function(){
+            if($(this).parent().hasClass('na')){
+                $('section.four ul li').addClass('na');
+                $(this).parent().removeClass('na');
+                var slideClass = $(this).parent().attr('class');
+                $('.slide').addClass('na');
+                $('.slide').removeClass('active');
+                if($('.slide#'+slideClass)){
+                    $('.back-four').addClass('na');
+                    $('#back-four-'+slideClass).removeClass('na');
+                    $('.slide#'+slideClass).removeClass('na');
+                    $('.slide#'+slideClass).addClass('active');
+                }
+            }
+        });
+        // button arrow navigation
+        $('section.four .control').click(function(){
+            var activeSlideId = parseInt($('.slide.active').attr('data-id'));
+            if($(this).hasClass('prev')){
+                activeSlideId--;
+            }else{
+                activeSlideId++;
+            }
+            if($('#slide'+activeSlideId).length>0){
+                $('section.four ul li').addClass('na');
+                $('section.four ul li.slide'+activeSlideId).removeClass('na');
+                $('.slide').addClass('na');
+                $('.slide').removeClass('active');
+                $('#slide'+activeSlideId).removeClass('na');
+                $('#slide'+activeSlideId).addClass('active');
+            }
+        });
+
+    // SLIDER TEAM
+    var delta = $('section .team-slider').width()*2;
+    function launchSlideTeam(dir){
+        if(dir){
+            TweenMax.to($('section .team-slider .back-team'), 120, {x:-delta, ease:Linear.easeNone,
+            onComplete:function(){
+                launchSlideTeam(false);
+            }}); 
+        }else{
+            TweenMax.to($('section .team-slider .back-team'), 120, {x:0, ease:Linear.easeNone,
+            onComplete:function(){
+                launchSlideTeam(true);
+            }});
+        }
+    }
+    if($('section .team-slider .back-team').length>0){
+        launchSlideTeam(true);
+    }
+
+    // GALERIE REFERENCE CLICK BUTTON
+    $('section.six .references-galerie .cross').click(function(){
+        $(this).parent().parent().parent().parent().addClass('active');
+    });
+    $('section.six .references-galerie .close').click(function(){
+        $(this).parent().parent().parent().parent().removeClass('active');
+    });
+
+});
